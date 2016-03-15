2434f6ab95197de22f74eac4fb12afc6301bbdcd
maraigue <main@hhiro.net>
2016-03-16 01:37:27 +0900

    2変数勾配降下法のコードが実装できていないままなので、サンプルコードとして上げるにあたって削除

--- multi-variable-minimization.cpp
+++ multi-variable-minimization.cpp
@@ -1,6 +1,6 @@
-// （2変数）ニュートン法・勾配降下法により関数の最小値を求める
+// （2変数）準ニュートン法により関数の最小値を求める
 // 
-// ※対象とする関数が凸かつ2階微分可能である必要がある
+// ※対象とする関数が凸かつ微分可能である必要がある
 
 #include <cfloat>
 #include <cmath>
@@ -15,10 +15,13 @@
 // ∂f/∂x = 2x + 2y + 4
 // ∂f/∂y = 2x + 6y + 5
 // 
-// dd_valでは、以下のものをまとめて行列にしたものを返す（ヘシアン）
+// 本当は、以下のものをまとめて行列にしたものも返さないとならない（ヘシアン）
 // ∂f^2/∂x^2 = 2
 // ∂f^2/∂y^2 = 6
 // ∂f^2/∂x∂y = 2
+// ただしここでは、ヘシアンを疑似的に求めることで最小値計算に利用する方法
+// （準ニュートン法）を利用しているため、ヘシアンを返す必要はなくなっている。
+// pseudo_inv_hessian が疑似的に求められたヘシアンである。
 
 class SampleFunction{
 public:
@@ -83,53 +86,8 @@ inline std::pair<Eigen::Vector2d, double> newton_2d_minimize(){
 	return newton_2d_minimize<Funcs>(std::sqrt(DBL_MIN));
 }
 
-/*
-template <class Funcs>
-std::pair<double, double> gradient_minimize(double x_min, double x_max, double precision){
-	double x = minimization_init(x_min, x_max);
-	double current_value = Funcs::val(x);
-	
-	for(;;){
-		// 動かす量
-		double shift = Funcs::d_val(x);
-		double shift_ratio = 1.0;
-		
-		// 更新途中のxの値と関数の値
-		double x_tmp;
-		double value_tmp;
-		
-		// 繰り返し値を更新する
-		for(;;){
-			x_tmp = x - shift * shift_ratio;
-			if(x_tmp < x_min) x_tmp = x_min;
-			if(x_tmp > x_max) x_tmp = x_max;
-			
-			value_tmp = Funcs::val(x_tmp);
-			if(value_tmp < current_value) break;
-			// もしxを動かして関数の値が増加してしまった場合は、
-			// xを動かす幅を半分にして再試行する。
-			// なお、xを動かす幅が所定の値を下回った場合はそこで打ち切り
-			shift_ratio *= 0.5;
-			if(std::fabs(shift * shift_ratio) < precision) break;
-		}
-		x = x_tmp;
-		if(std::fabs(current_value - value_tmp) < precision) break;
-		current_value = value_tmp;
-	}
-	return std::make_pair(x, current_value);
-}
-
-template <class Funcs>
-inline std::pair<double, double> gradient_minimize(double x_min, double x_max){
-	return gradient_minimize<Funcs>(x_min, x_max, std::sqrt(DBL_MIN));
-}
-*/
-
 int main(void){
 	std::pair<Eigen::Vector2d, double> result = newton_2d_minimize<SampleFunction>();
-	//std::pair<double, double> result = gradient_minimize<SampleFunction>();
 	std::cout << "x = " << result.first << std::endl;
 	std::cout << "f(x) = " << result.second << std::endl;
 }
-
-
